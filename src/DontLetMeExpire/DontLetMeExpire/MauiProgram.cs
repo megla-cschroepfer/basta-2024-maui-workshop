6b9ad88e5e21c347e67784fb8e7f17fb78743b90
Andre Kraemer <andre@andrekraemer.de>
2024-09-15 15:39:31 +0200

    12: Ende

--- src/DontLetMeExpire/DontLetMeExpire/MauiProgram.cs
+++ src/DontLetMeExpire/DontLetMeExpire/MauiProgram.cs
@@ -17,6 +17,7 @@ namespace DontLetMeExpire
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("MaterialSymbols-Rounded.ttf", "MaterialSymbolsRounded");
                 });
 
 #if DEBUG
